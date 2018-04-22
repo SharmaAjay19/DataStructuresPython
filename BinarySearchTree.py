24d1d2c0007066370a9e331845e7d722cc078533
Ajay Sharma <ajsharm@microsoft.com>
2018-04-22 19:19:40 +0530

    Added Binary Search Tree

---
+++ BinarySearchTree.py
@@ -0,0 +1,67 @@
+class BST:
+	def __init__(self, value):
+		self.val = value
+		self.left = None
+		self.right = None
+	def insert(self, value):
+		if value <= self.val:
+			if self.left:
+				self.left.insert(value)
+			else:
+				self.left = BST(value)
+		else:
+			if self.right:
+				self.right.insert(value)
+			else:
+				self.right = BST(value)
+	def search(self, value):
+		if self.val == value:
+			return True
+		elif self.val < value:
+			if self.right:
+				return self.right.search(value)
+			else:
+				return False
+		else:
+			if self.left:
+				return self.left.search(value)
+			else:
+				return False
+	def inorder(self):
+		result = []
+		if self.left:
+			result += self.left.inorder()
+		result += [self.val]
+		if self.right:
+			result += self.right.inorder()
+		return result
+	def preorder(self):
+		result = [self.val]
+		if self.left:
+			result += self.left.preorder()
+		if self.right:
+			result += self.right.preorder()
+		return result
+	def postorder(self):
+		result = []
+		if self.left:
+			result += self.left.postorder()
+		if self.right:
+			result += self.right.postorder()
+		result += [self.val]
+		return result
+
+tree = BST(20)
+tree.insert(10)
+tree.insert(25)
+tree.insert(300)
+tree.insert(22)
+tree.insert(5)
+tree.insert(-10)
+tree.insert(12)
+tree.insert(8)
+print("SEARCH 10", tree.search(10))
+print("SEARCH 29", tree.search(29))
+print("INORDER", tree.inorder())
+print("PREORDER", tree.preorder())
+print("POSTORDER", tree.postorder())
